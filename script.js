b8138e983245db8ddd50177686a1a3b9a0fee95c
Artem Vanty <artem.vanty.0358@gmail.com>
2022-11-29 22:30:44 +0900

    add imgs

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+function abs    (x)   { return Math.abs      (x)   }
+function acos   (x)   { return Math.acos     (x)   }
+function acosh  (x)   { return Math.acosh    (x)   }
+function asin   (x)   { return Math.asin     (x)   }
+function asinh  (x)   { return Math.asinh    (x)   }
+function atan   (x)   { return Math.atan     (x)   }
+function atan2  (x,n) { return Math.atan2    (x,n) }
+function atanh  (x)   { return Math.atanh    (x)   }
+function cos    (x)   { return Math.cos      (x)   }
+function cosh   (x)   { return Math.cosh     (x)   }
+function exp    (x)   { return Math.exp      (x)   }
+function log    (x)   { return Math.log      (x)   }
+function random (x)   { return Math.random   (x)   }
+function sin    (x)   { return Math.sin      (x)   }
+function sinh   (x)   { return Math.sinh     (x)   }
+function sqrt   (x)   { return Math.sqrt     (x)   }
+function tan    (x)   { return Math.tan      (x)   }
+function tanh   (x)   { return Math.tanh     (x)   }
+
+var PI = Math.PI;
+
 function fun1(x) 
 {
     return eval(document.getElementById("exprsn").value);  
@@ -38,9 +59,10 @@ function funGraph (ctx,axes,func,color,thick)
 function draw()
 {
     var canvas = document.getElementById("canvas");
+    var excepted = document.getElementById("excepted");
     if (null==canvas || !canvas.getContext) return;
 
-    var axes={}, ctx=canvas.getContext("2d");
+    var axes={}, ctx=canvas.getContext("2d"),ectx=excepted.getContext("2d");
     ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height)
     axes.x0 = .5 + .5*canvas.width;  // x0 pixels from left to x=0
     axes.y0 = .5 + .5*canvas.height; // y0 pixels from top to y=0
@@ -48,5 +70,8 @@ function draw()
     axes.doNegativeX = true;
 
     showAxes(ctx,axes);
-    funGraph(ctx,axes,fun1,"rgb(255,64,64)",2); 
+    funGraph(ctx,axes,fun1,"rgb(255,64,64)",2);
+    const img = new Image();   // Create new img element
+    img.src = 'file:///1.png';
+    ectx.drawImage(img, 0, 0,ectx.canvas.width,ectx.canvas.height);
 }
